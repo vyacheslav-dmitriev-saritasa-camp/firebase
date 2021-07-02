339378dc6d6f94536a50cdd47776db3c20edbc02
Vyacheslav Dmitriev <viacheslav.dmitriev@saritasa.com>
2021-07-02 11:54:05 +0700

    1st v of the pagination

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const sortByButton = document.querySelector("button#sortBy");
 const input = document.querySelector("#search");
 const selectSearch = document.querySelector("#selectSearch");
 
-function initialApp() {
+(function initialApp() {
     const isAuth = getLoginStatus();
     const headerLogin = document.querySelector(".header__login");
 
@@ -32,9 +32,9 @@ function initialApp() {
             createBlocksIntoHTML(isAuth, films);
         });
     }
-}
+})();
 
-initialApp();
+// initialApp();
 
 function debounce(func, wait, immediate) {
     let timeout;
@@ -269,54 +269,105 @@ const searchFilms = async (selectSearchValue, inputSearch) => {
 };
 
 const createBlocksIntoHTML = (isAuth, films) => {
-    wrapper.innerHTML = "";
+    const list_element = document.querySelector("#list");
+    const pagination_element = document.querySelector("#pagination");
 
-    const head = ["number", "title", "director", "producer", "edited"];
+    let current_page = 1;
+    let rows = 4;
 
-    head.forEach(el => {
-        const div = document.createElement("div");
+    function DisplayList(isAuth, items, wrapper, rows_per_page, page) {
+        wrapper.innerHTML = "";
+        const head = ["number", "title", "director", "producer", "edited"];
 
-        div.innerHTML = `
+        head.forEach(el => {
+            const div = document.createElement("div");
+
+            div.innerHTML = `
             <div class='head item'>
                 <h2 id=${el}>${el}</h2>
             </div>`;
 
-        wrapper.append(div);
-    });
+            wrapper.appendChild(div);
+        });
 
-    films.forEach(el => {
-        const { pk: number } = el;
-        const { fields } = el;
+        page--;
 
-        const array = [
-            number,
-            fields.title,
-            fields.director,
-            fields.producer,
-            fields.edited,
-        ];
+        let start = rows_per_page * page;
+        let end = start + rows_per_page;
+        let paginatedItems = items.slice(start, end);
 
-        array.forEach(el => {
-            if (isAuth && Number.isInteger(el)) {
-                const div = document.createElement("div");
-                div.innerHTML = `
+        for (let i = 0; i < paginatedItems.length; i++) {
+            let item = paginatedItems[i];
+            console.log(item);
+
+            const { pk: number } = item;
+            const { fields } = item;
+            const array = [
+                number,
+                fields.title,
+                fields.director,
+                fields.producer,
+                fields.edited,
+            ];
+            array.forEach(el => {
+                if (isAuth && Number.isInteger(el)) {
+                    const div = document.createElement("div");
+                    div.innerHTML = `
                 <div class='item'>
                     <h3>
                         <a href='http://127.0.0.1:5500/src/pages/authorized/film-info.html?film-number=${el}' target='_blank'>${el}</a>
                     </h3>
                 </div>`;
-                wrapper.append(div);
-            } else {
-                const div = document.createElement("div");
+                    wrapper.append(div);
+                } else {
+                    const div = document.createElement("div");
 
-                div.innerHTML = `
+                    div.innerHTML = `
                     <div class='item'>
                         <h3>${el}</h3>
                     </div>`;
-                wrapper.append(div);
-            }
+                    wrapper.append(div);
+                }
+            });
+        }
+    }
+
+    function SetupPagination(isAuth, items, wrapper, rows_per_page) {
+        wrapper.innerHTML = "";
+
+        let page_count = Math.ceil(items.length / rows_per_page);
+        for (let i = 1; i < page_count + 1; i++) {
+            let btn = PaginationButton(isAuth, i, items);
+            wrapper.appendChild(btn);
+        }
+    }
+
+    function PaginationButton(isAuth, page, items) {
+        const button = document.createElement("button");
+
+        button.classList.add("pagination");
+
+        button.innerText = page;
+
+        if (current_page == page) {
+            button.classList.add("active");
+        }
+
+        button.addEventListener("click", () => {
+            current_page = page;
+            DisplayList(isAuth, items, list_element, rows, current_page);
+
+            let current_btn = document.querySelector("button.active");
+            current_btn.classList.remove("active");
+
+            button.classList.add("active");
         });
-    });
+
+        return button;
+    }
+
+    DisplayList(isAuth, films, list_element, rows, current_page);
+    SetupPagination(isAuth, films, pagination_element, rows);
 };
 
 async function loadFilms() {
